620979f713bc19315f16092235af355718435f88
brkzsp <kauebragagg@gmail.com>
2021-07-05 22:56:43 -0300

    vanila

--- snake-game/script.js
+++ snake-game/script.js
@@ -13,13 +13,13 @@ let food ={
 }
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "#291b7b" ;
     context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
 function criarCobrinha (){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "white";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
